31f9cf62298fe84e0c27a4cf0e145fc09fcae521
ale20331127 <110247125+ale20331127@users.noreply.github.com>
2023-11-27 07:09:22 -0600

    Delete TareaProgra26Nov directory

--- TareaProgra26Nov/proyecto/app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Productos, Proveedores
-
-admin.site.register(Productos)
-admin.site.register(Proveedores)
--- TareaProgra26Nov/proyecto/app/formularios/add_produc.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class Add_produ(forms.Form):
-    nombre = forms.CharField(max_length=100)
-    stonk = forms.IntegerField()
-    
